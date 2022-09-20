de795c2e92f87c965822addc11aab483c0390c39
Afrizal <4friizal@gmail.com>
2022-09-20 21:52:13 +0700

    landing page push

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      mulish: ["Mulish", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
